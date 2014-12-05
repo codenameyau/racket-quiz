fc237e7a292e014c126415fd98735c5494d81a19
Jorge Yau <jyau01@citymail.cuny.edu>
2014-12-04 23:18:21 -0500

    Adding gitignore and banned

---
+++ quiz/9-banned-words/banned-words.rkt
@@ -0,0 +1,13 @@
+;; Ruby Quiz 9 - Banned Words
+;; https://github.com/codenameyau/racket-quiz
+;; 
+;; Description:
+;;   Typically a Bayes-Filter is used to probabilistic
+;;   detect spam emails. Make a simple spam detector.
+;;
+#lang racket
+
+(define (check-banned word)
+  1)
+
+(check-banned "word")
